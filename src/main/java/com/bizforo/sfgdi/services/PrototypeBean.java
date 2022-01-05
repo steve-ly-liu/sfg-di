72073a57965b0e8eca9fa14b3e59cbb1c957cdf0
steve.ly.liu@gmail.com <steve.ly.liu@gmail.com>
2022-01-04 22:45:05 -0600

    add @Scope annotation to PrototypeBean and SingletonBean

--- src/main/java/com/bizforo/sfgdi/services/PrototypeBean.java
+++ src/main/java/com/bizforo/sfgdi/services/PrototypeBean.java
@@ -1,7 +1,10 @@
 package com.bizforo.sfgdi.services;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope((ConfigurableBeanFactory.SCOPE_PROTOTYPE))
 @Component
 public class PrototypeBean {
 
--- src/main/java/com/bizforo/sfgdi/services/SingletonBean.java
+++ src/main/java/com/bizforo/sfgdi/services/SingletonBean.java
@@ -1,7 +1,10 @@
 package com.bizforo.sfgdi.services;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 @Component
 public class SingletonBean {
     public SingletonBean() {
